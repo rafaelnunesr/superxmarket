260c2013806816bb09f09fd4e0ff0e10f04f4a78
rafaelnunesr <rafaelrafssk@hotmail.com>
2021-02-12 19:13:39 -0300

    finish preview signup screen

--- superxmarket/Screens/SignUp/SignUpViewController.swift
+++ superxmarket/Screens/SignUp/SignUpViewController.swift
@@ -61,6 +61,9 @@ class SignUpViewController: UIViewController {
         self.setupPhoneNumber()
         self.setupBirthday()
         self.setupAddressTextField()
+        self.setupCheckbox()
+        self.setupTermsAndConditionsLabel()
+        self.setupSignupButton()
     }
     
     private func setupHeader() {
@@ -183,6 +186,73 @@ class SignUpViewController: UIViewController {
         constraints.append(addressTextField.topAnchor.constraint(equalTo: birthdayTextField.bottomAnchor, constant: 16))
         constraints.append(addressTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16))
         constraints.append(addressTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16))
+        
+        NSLayoutConstraint.activate(constraints)
+    }
+    
+    private func setupCheckbox() {
+        addCheckboxConstraints()
+        
+        self.checkbox.layer.cornerRadius = 10
+        self.checkbox.backgroundColor = .white
+        self.checkbox.layer.shadowOffset = CGSize(width: 0, height: 3.0)
+        self.checkbox.layer.shadowOpacity = 0.3
+        self.checkbox.layer.shadowRadius = 4.0
+        self.checkbox.layer.shadowColor = UIColor.black.cgColor
+        
+    }
+    
+    func addCheckboxConstraints() {
+        let size: CGFloat = 30
+        
+        checkbox.translatesAutoresizingMaskIntoConstraints = false
+        
+        var constraints = [NSLayoutConstraint]()
+        
+        constraints.append(checkbox.topAnchor.constraint(equalTo: addressTextField.bottomAnchor, constant: 16))
+        constraints.append(checkbox.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16))
+        constraints.append(checkbox.widthAnchor.constraint(equalToConstant: size))
+        constraints.append(checkbox.heightAnchor.constraint(equalToConstant: size))
+        
+        NSLayoutConstraint.activate(constraints)
+        
+    }
+    
+    private func setupTermsAndConditionsLabel() {
+        addTermsAndConditionsLabelConstraints()
+        
+        self.termsConditionsLabel.text = "I agree with terms and conditions"
+    }
+    
+    func addTermsAndConditionsLabelConstraints() {
+        termsConditionsLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        var constraints = [NSLayoutConstraint]()
+        
+        constraints.append(termsConditionsLabel.centerYAnchor.constraint(equalTo: checkbox.centerYAnchor))
+        constraints.append(termsConditionsLabel.leadingAnchor.constraint(equalTo: checkbox.trailingAnchor, constant: 16))
+        constraints.append(termsConditionsLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16))
+        
+        NSLayoutConstraint.activate(constraints)
+    }
+    
+    private func setupSignupButton() {
+        addSignupButtonConstraints()
+        
+        self.signupButton.backgroundColor = Colors.mainPurple
+        self.signupButton.setTitle("Sign Up", for: .normal)
+        self.signupButton.tintColor = .white
+    }
+    
+    func addSignupButtonConstraints() {
+        signupButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        var constraints = [NSLayoutConstraint]()
+        
+        constraints.append(signupButton.topAnchor.constraint(equalTo: checkbox.bottomAnchor, constant: 16))
+        constraints.append(signupButton.leadingAnchor.constraint(equalTo: checkbox.trailingAnchor, constant: 16))
+        constraints.append(signupButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16))
+        constraints.append(signupButton.heightAnchor.constraint(equalToConstant: 52))
         
         NSLayoutConstraint.activate(constraints)
     }
